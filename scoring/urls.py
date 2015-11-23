fcdb4dffbeab788a3ad3905d6a445e1d8ef5bb65
Jacob Ward <jacobward1898@gmail.com>
2015-11-23 15:37:20 -0700

    favicon added and svg

--- scoring/urls.py
+++ scoring/urls.py
@@ -1,9 +1,19 @@
 from django.conf.urls import url
 
+from django.contrib.staticfiles.storage import staticfiles_storage
+from django.views.generic.base import RedirectView
+
 from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^tech/?$', views.tech, name='tech'),
     url(r'^json/(.*)$', views.json, name='json'),
+    url(
+        r'^favicon.ico$',
+        RedirectView.as_view(
+            url=staticfiles_storage.url('favicon.ico'),
+            permanent=False),
+        name="favicon"
+    )
 ]
